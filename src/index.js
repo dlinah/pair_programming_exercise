16453b0eea33e2d58be380fb68789e2a61f47b76
lina <linah.ahmed.92@gmail.com>
2020-09-09 11:48:07 +0200

    exercise-1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App =()=>(<div>react app</div>)
+import AppContainer from './common/containers/App';
+import './styles/_main.scss';
+import Routes from './routes';
 
 ReactDOM.render(
-  <App/>,
+  <AppContainer>
+    <Routes />
+  </AppContainer>,
   document.getElementById('root')
 );
